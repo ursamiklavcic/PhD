3f30254dd88d4d1fcec37afb1c8d1e645a479cba
Urša Miklavčič <ursa.miklavcic@gmail.com>
2025-10-13 12:37:37 +0000

    Persistence of ARGs through time

--- code/longitudinal_shotgun/AMRfinder.R
+++ code/longitudinal_shotgun/AMRfinder.R
@@ -18,7 +18,8 @@ metadata <- read_csv2('~/projects/longitudinal_shotgun/data/metadata.csv')
 amr_pre <- read_tsv('~/projects/longitudinal_shotgun/data/AMR/AMRfinder/amrfinder_results.tsv') %>%
   rename('contig_id' = `Contig id`, 'ARG' = `Element symbol`) %>%
   filter(`% Coverage of reference` > 80 & `% Identity to reference` > 50) %>%  
-  select(contig_id, ARG, Class)
+  filter(Type == 'AMR') %>% 
+  select(contig_id, ARG, Class) 
 
 unique(amr_pre$ARG)
 
@@ -159,7 +160,7 @@ ggsave('out/ARGs/unique_tpm_args_mechanism.svg', dpi = 600)
 # Taking into account events 
 event_data <- read.table('data/extreme_event_data.csv', sep = ',', header = TRUE) 
 
-amr_simplyfied %>%
+unique_time <- amr_simplyfied %>%
   group_by(person, day, Class) %>%
   reframe(sum_TPM = sum(TPM), extremevent_type) %>%
   ggplot(aes(x = day, y = sum_TPM)) +
@@ -173,7 +174,7 @@ amr_simplyfied %>%
 ggsave('out/ARGs/AMRf_extreme_event_TPM.png')
 
 # Number of different ARGs per individual through time 
-amr_simplyfied %>% filter(TPM > 0) %>%
+tpm_time <- amr_simplyfied %>% filter(TPM > 0) %>%
   group_by(person, day, Class) %>%
   summarise(sum_unique = n_distinct(ARG), .groups = 'drop') %>%
   filter(sum_unique > 0) %>%
@@ -186,6 +187,14 @@ amr_simplyfied %>% filter(TPM > 0) %>%
   facet_wrap(~person, scales = 'free_y') +
   labs(y = '# unique genes', x = 'Day', color = 'Class of ARG', fill = 'Event type') 
 ggsave('out/ARGs/AMRf_unique_class_ARG.png')
+
+
+ggarrange(unique_time + labs(tag = 'A'), 
+          tpm_time + labs(tag = 'B'), 
+          common.legend = T, 
+          legend = 'bottom', 
+          ncol = 1)
+ggsave('out/ARGs/unique_tpm_time_up.svg', dpi=600)
 
 # What is the taxonomy of different ARG classes 
 amr_tax <- amr_simplyfied %>%
@@ -234,41 +243,7 @@ ggarrange(unique_tax +labs(tag = 'A'), tpm_tax + labs(tag = 'B'),
           common.legend = T, legend =  'bottom')
 ggsave('out/ARGs/AMRf_unique_tpm_ARGs_taxonomy.png', dpi = 600)
 
-## 
-# Persistence of ARgs within an individual 
-person_day <- amr_simplyfied %>%
-  group_by(person) %>% 
-  reframe(all_timepoints = n_distinct(day))
-
-present <- amr_simplyfied %>% 
-  group_by(person, ARG, day, Class) %>%
-  reframe(PA = ifelse(sum(TPM) > 0, 1, 0)) %>% 
-  group_by(person, ARG, Class) %>%
-  reframe(timepoint_present = sum(PA), 
-          timepoint_missing = sum(PA = 0)) %>% 
-  left_join(person_day, by = 'person') %>%  
-  mutate(prevalence = (timepoint_present/all_timepoints)*100) %>% 
-  filter(prevalence > 0) %>%  
-  group_by(person) %>% 
-  mutate(all_args = n_distinct(ARG)) %>% 
-  ungroup() %>% 
-  group_by(person, Class, prevalence) %>% 
-  reframe(n_args_class = n_distinct(ARG), 
-          percent_args = (n_args_class/all_args) *100) 
-  
-present %>% 
-  ggplot(aes(y = percent_args, x = prevalence, color = person)) +
-  geom_point(size = 2) +
-  geom_line(linewidth = 1.2) +
-  #geom_smooth(se = F) +
-  scale_x_continuous(breaks = seq(0, 100, by = 25)) +
-  labs(y = '', x = '# timepoints an ARG was found', color = 'Individual') +
-  facet_wrap(~Class, scales = 'free_x')
-ggsave('out/ARGs/AMRf_args_present.png')
-
-
-missing <- filter(present, prevalence < 90) 
-
+# All genes through time 
 amr %>%
   group_by(ARG, person, time_point, Class) %>%
   reframe(TPM = sum(TPM)) %>%
@@ -279,6 +254,7 @@ amr %>%
   facet_wrap(~person, scales = 'free_y')
 ggsave('out/ARGs/tpm_time_ARGs.png')
 
+# Mechanisms through time
 amr %>%
   group_by(person, time_point, mechanism_resistence) %>%
   reframe(TPM = sum(TPM)) %>%
@@ -318,4 +294,130 @@ amr_alpha %>%
   geom_point(size = 2, aes(color = Class)) +
   facet_wrap(~person, scales = 'free')
 ggsave('out/ARGs/tpm_shannon.png')
+
+# CTX-M1
+amr %>% filter(ARG == 'blaCTX-M-1') %>% 
+  group_by(day, person, ARG) %>% 
+  reframe(TPM = sum(TPM)) %>% 
+  filter(TPM > 0.5) %>% 
+  ggplot(aes(x = day, y = TPM, color = person)) +
+  geom_point(size = 2) +
+  geom_line(linewidth=1.2) +
+  facet_wrap(~person) +
+  theme(legend.position = 'none') +
+  labs(x = 'Day')
+ggsave('out/ARGs/ctx-m1.png')  
+
+
+unique(filter(amr, ARG == 'blaCTX-M-1', TPM > 0.5)$Tax)
+
+# Van resistence 
+van <- amr %>%  
+  mutate(van = case_when(str_detect(ARG, 'van') ~ 'van', 
+                         TRUE ~ 'not van')) %>% 
+  filter(van == 'van') 
+
+unique(filter(van, TPM > 0.5)$Tax)
+
+van %>% 
+  group_by(day, person, ARG, Tax) %>% 
+  reframe(TPM = sum(TPM)) %>% 
+  filter(TPM > 0.5) %>%  
+  ggplot(aes(x = day, y = TPM, color = Tax)) +
+  geom_point(size = 2) +
+  geom_line(linewidth=1.2) +
+  scale_y_log10() +
+  facet_wrap(ARG~person) +
+  labs(x = 'Day')
+ggsave('out/ARGs/van.png')
+
+
+## 
+# Persistence of ARgs within an individual 
+person_day <- amr %>%
+  group_by(person) %>% 
+  reframe(all_timepoints = n_distinct(day))
+
+present <- amr %>% 
+  group_by(person, ARG, day, Class) %>%
+  reframe(PA = ifelse(sum(TPM) > 0, 1, 0)) %>% 
+  group_by(person, ARG, Class) %>%
+  reframe(timepoint_present = sum(PA), 
+          timepoint_missing = sum(PA = 0)) %>% 
+  left_join(person_day, by = 'person') %>%  
+  mutate(prevalence = (timepoint_present/all_timepoints)*100) %>% 
+  filter(prevalence > 0) %>%  
+  group_by(person) %>% 
+  mutate(all_args = n_distinct(ARG)) %>% 
+  ungroup() %>% 
+  group_by(person, Class, prevalence) %>% 
+  reframe(n_args_class = n_distinct(ARG), 
+          percent_args = (n_args_class/all_args) *100) 
+
+present %>% 
+  ggplot(aes(y = n_args_class, x = prevalence, color = person)) +
+  geom_point(size = 2) +
+  geom_line(linewidth = 1.2) +
+  #geom_smooth(se = F) +
+  scale_x_continuous(breaks = seq(0, 100, by = 25)) +
+  scale_y_continuous(breaks = seq(0,8, by = 1)) +
+  labs(y = '', x = '# timepoints an ARG was found', color = 'Individual') +
+  facet_wrap(~Class, scales = 'free')
+ggsave('out/ARGs/AMRf_args_present.png')
+
+
+present %>% filter(n_args_class > 1)
+
+# Heatmap genes x time for each person
+# Only ARGs present in more than 1 timepoint and not always
+amr %>%  
+  group_by(person, ARG, day, Class) %>%
+  reframe(PA = ifelse(sum(TPM) > 0, 1, 0)) %>% 
+  group_by(person, ARG, Class)  %>%  
+  mutate(sum = sum(PA)) %>% 
+  filter(sum > 1) %>% 
+  filter(sum < 11) %>% 
+  ggplot(aes(x = as.factor(day), y = ARG, fill = as.factor(PA))) +
+  geom_tile() +
+  scale_fill_manual(values = c('white', 'blue')) +
+  facet_wrap(~person, scales = 'free')
+ggsave('out/ARGs/more_than_once_but_not_always.png')
+
+
+# Always present ARGs 
+amr %>%  
+  group_by(person, ARG, day, Class) %>%
+  reframe(PA = ifelse(sum(TPM) > 0, 1, 0)) %>% 
+  group_by(person, ARG, Class)  %>%  
+  mutate(sum = sum(PA)) %>% 
+  filter(sum > 11) %>% 
+  ggplot(aes(x = as.factor(day), y = ARG, fill = as.factor(PA))) +
+  geom_tile() +
+  scale_fill_manual(values = c('white', 'blue')) +
+  facet_wrap(~person, scales = 'free')
+ggsave('out/ARGs/always_present.png')
+
+# How many ARGs are always present ? 
+amr_persistence <- amr %>%  
+  group_by(person, ARG, day, Class, TPM) %>%
+  reframe(PA = ifelse(sum(TPM) > 0, 1, 0)) %>% 
+  group_by(person, ARG, Class)  %>%  
+  mutate(sum = sum(PA)) 
+
+# Diiferent ARGS per person
+amr %>% filter(TPM > 0) %>% 
+  group_by(person) %>%  
+  reframe(len = n_distinct(ARG))
+
+# How many ARGs are always present ? 
+amr_persistence %>% 
+  filter(sum > 11) %>% 
+  group_by(person) %>%  
+  reframe(len = n_distinct(ARG))
+
+# How many ARGs are only present at 1 timepoint? 
+amr_persistence %>%  
+  filter(sum < 3) %>%  
+  group_by(person) %>%  
+  reframe(len = n_distinct(ARG))
 
